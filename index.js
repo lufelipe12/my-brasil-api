673d6687afa0109297c21dd092b8666a0ba84862
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-03-10 10:27:37 -0300

    Feature: api made to treat data from other api

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const api = require("./Services");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+const port = 3000;
+
+app.get("/endereco/:cnpj", async (req, res) => {
+  await api.get(`/api/cnpj/v1/${req.params.cnpj}`).then((response) => {
+    const { uf, cep, bairro, numero, complemento } = response.data;
+    const treatedData = {
+      uf: uf,
+      cep: cep,
+      bairro: bairro,
+      numero: numero,
+      complemento: complemento,
+    };
+    res.status(200).send(treatedData);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Express started at http://localhost:${port}`);
+});
